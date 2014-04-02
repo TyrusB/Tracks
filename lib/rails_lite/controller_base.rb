6ac6cc05a342379ed44fa23b7ff275fa970cafc5
Ty Brooks <brooks85.ty@gmail.com>
2014-04-01 22:14:49 -0700

    Update controller_base.rb

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -26,9 +26,6 @@ class ControllerBase
     end
   end
 
-  # populate the response with content
-  # set the responses content type to the given type
-  # later raise an error if the developer tries to double render
   def render_content(content, type)
     @res.body = content
     @res.content_type = type
@@ -40,12 +37,10 @@ class ControllerBase
     self.flash.store_flash unless self.flash.destroy_now?
   end
 
-  # helper method to alias @already_rendered
   def already_rendered?
     !!@already_built_response
   end
 
-  # set the response status code and header
   def redirect_to(url)
     @res.status = 302
     @res["location"] = url
@@ -57,8 +52,7 @@ class ControllerBase
     self.flash.store_flash unless self.flash.destroy?
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
+  # ERB implementation using binding
   def render(template_name)
     data = File.read("views/#{self.class.to_s.underscore}/#{template_name.to_s}.html.erb")
     erb = ERB.new(data).result(binding)
@@ -71,7 +65,7 @@ class ControllerBase
     @session ||= Session.new(@req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
+  # calls action name for router
   def invoke_action(name)
     self.send(name)
     render(name) unless already_rendered?
