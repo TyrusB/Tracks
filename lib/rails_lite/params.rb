c4cc2449897712c13b771c6c6dd42b1d1779eab7
Ty Brooks <brooks85.ty@gmail.com>
2014-04-01 22:09:46 -0700

    Update params.rb

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -1,10 +1,7 @@
 require 'uri'
 
 class Params
-  # use your initialize to merge params from
-  # 1. query string
-  # 2. post body
-  # 3. route params
+
   def initialize(req, route_params = {})
     @params = {}
 
@@ -27,15 +24,6 @@ class Params
     @params[key]
   end
 
-  def permit(*keys)
-  end
-
-  def require(key)
-  end
-
-  def permitted?(key)
-  end
-
   def to_s
     @params.to_s
   end
@@ -43,11 +31,7 @@ class Params
   class AttributeNotFoundError < ArgumentError; end;
 
   private
-  # this should return deeply nested hash
-  # argument format
-  # user[address][street]=main&user[address][zip]=89436
-  # should return
-  # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
+
   def parse_www_encoded_form(www_encoded_form)
     params = {}
 
@@ -71,8 +55,6 @@ class Params
     params
   end
 
-  # this should return an array
-  # user[address][street] should return ['user', 'address', 'street']
   def parse_key(key)
     key.split(/\]\[|\[|\]/)
   end
